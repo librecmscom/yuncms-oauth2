fdaa571a06fb13d49a57baf687cadd8969b6bb71
Peter <xutongle@gmail.com>
2017-11-14 19:54:59 +0800

    init

---
+++ Module.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @link https://github.com/borodulin/yii2-oauth2-server
+ * @copyright Copyright (c) 2015 Andrey Borodulin
+ * @license https://github.com/borodulin/yii2-oauth2-server/blob/master/LICENSE
+ */
+
+namespace yuncms\oauth2;
+
+use Yii;
+
+/**
+ * @author Andrey Borodulin
+ */
+class Module extends \yii\base\Module
+{
+
+    public $behaviors;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        if (!empty($this->behaviors)) {
+            return $this->behaviors;
+        } else {
+            return parent::behaviors();
+        }
+    }
+
+    /**
+     * 注册语言包
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        if (!isset(Yii::$app->i18n->translations['oauth2*'])) {
+            Yii::$app->i18n->translations['oauth2*'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+}
